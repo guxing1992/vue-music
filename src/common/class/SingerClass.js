7adf7d4f7ffb51616748509ab8d791f5b3e528a1
guxing1992 <1392497812@qq.com>
2018-12-01 22:24:03 +0800

    singer-format-data

---
+++ src/common/class/SingerClass.js
@@ -0,0 +1,11 @@
+import pinyin from 'js-pinyin'
+pinyin.setOptions({checkPolyphone: false, charCase: 0})
+export default class Singer {
+  constructor ({country, singerId, singerName, singerPic}) {
+    this.country = country
+    this.singer_id = singerId
+    this.singer_name = singerName
+    this.singer_pic = singerPic
+    this.findex = pinyin.getCamelChars(singerName)[0]
+  }
+}
--- src/components/pages/singer/Singer.vue
+++ src/components/pages/singer/Singer.vue
@@ -7,6 +7,9 @@
 <script>
 import {getSingerList} from '@/api/singer'
 import {ERR_OK} from '@/api/config'
+import Singer from '@/common/class/SingerClass'
+const HOT_NAME = '热门'
+const HOT_SINGER_LENGTH = 10
 export default {
   data () {
     return {
@@ -14,17 +17,57 @@ export default {
     }
   },
   created () {
-    this._getSingerList(2)
+    this._getSingerList(-100)
   },
   methods: {
     _getSingerList (index) {
+      var templist = []
       getSingerList(index).then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res)
           res = res.singerList.data.singerlist
-          this.singerlist = res
+          res.forEach((item, index) => {
+            templist.push(
+              new Singer({country: item.country, singerId: item.singer_id, singerName: item.singer_name, singerPic: item.singer_pic}))
+          })
+          this.singerlist = this._normalizeSinger(templist)
         }
       })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LENGTH) {
+          map.hot.items.push(item)
+        }
+        const key = item.findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(item)
+      })
+      // 得到有序列表
+      let hot = []
+      let rest = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          rest.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      rest.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(rest)
     }
   }
 }
