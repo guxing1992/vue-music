37cbceb673344c116589bd5c7730998869b960e5
guxing1992 <1392497812@qq.com>
2018-12-07 17:05:18 +0800

    finish-chapeter5

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -23,6 +23,12 @@
                 <li :class="{'current':currentIndex===index}" v-for="(item,index) in shortcutList" :key="index" :data-index="index" class="item">{{item}}</li>
             </ul>
         </div>
+        <div class="list-fixed" v-show="fixedTitle" ref="fixed">
+          <h1 class="fixed-title">{{fixedTitle}}</h1>
+        </div>
+        <div v-show="!data.length" class="loading-container">
+          <loading></loading>
+        </div>
     </div>
 
 </template>
@@ -30,8 +36,9 @@
 <script>
 import Scorll from '../scroll/scroll'
 import {getData} from '@/common/js/dom'
+import Loading from '../loading/loading'
 const ANCHOR_HEIGHT = 18
-// const TITLE_HEIGHT = 30
+const TITLE_HEIGHT = 30
 export default {
   data () {
     return {
@@ -49,10 +56,14 @@ export default {
       }
     }
   },
-  components: {Scorll},
+  components: {Scorll, Loading},
   computed: {
     shortcutList () {
       return this.data.map((group) => group.title.substr(0, 1))
+    },
+    fixedTitle () {
+      if (this.scrollY > 0) return ''
+      return this.data[this.currentIndex] ? this.data[this.currentIndex].title : ''
     }
   },
   methods: {
@@ -76,7 +87,17 @@ export default {
       this.scrollY = pos.y
     },
     _scrollTo (index) {
-      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0)
+      console.log(index)
+      if (!index && index !== 0) {
+        return
+      }
+      if (index < 0) {
+        index = 0
+      } else if (index > this.listHeight.length - 2) {
+        index = this.listHeight.length - 2
+      }
+      this.scrollY = -this.listHeight[index]
+      this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 500)
     },
     _calculateHeight () {
       this.listHeight = []
@@ -102,6 +123,7 @@ export default {
       }, 20)
     },
     scrollY (newY) {
+      // console.log(newY)
       const listHeight = this.listHeight
       // 当滚动到顶部，newY>0
       if (newY > 0) {
@@ -114,13 +136,19 @@ export default {
         let height2 = listHeight[i + 1]
         if (-newY >= height1 && -newY < height2) {
           this.currentIndex = i
+          this.diff = height2 + newY
           return
         }
       }
       // 当滚动到底部，且-newY大于最后一个元素的上限
       this.currentIndex = listHeight.length - 2
+    },
+    diff (newVal) {
+      let fixedTop = (newVal > 0 && newVal < TITLE_HEIGHT) ? (newVal - TITLE_HEIGHT) : 0
+      if (this.fixedTop === fixedTop) return
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
     }
-
   }
 }
 
@@ -176,4 +204,21 @@ export default {
         font-size: $font-size-small
         &.current
           color: $color-theme
+  .list-fixed
+    position:absolute;
+    top:0;
+    width:100%;
+    z-index: 100;
+    .fixed-title
+      height: 30px
+      line-height: 30px
+      padding-left: 20px
+      font-size: $font-size-small
+      color: $color-text-l
+      background: $color-highlight-background
+  .loading-container
+    position:absolute;
+    top:50%;
+    transform :translateY(-50%);
+    width:100%;
 </style>
